ba46f8f2088851bc8a7c2dec1732264199cd552d
mannaluca02 <mannaluca02@gmail.com>
2024-11-01 17:44:24 +0100

    mqtt works. Need to display better. REQUIREMENTS NEED TO BE UPDATED. Used not privided broker since it did not work, check congif-switch.ini file. make sure to add simulator.log.1

--- grp3/infrastructure/webservice_http.py
+++ grp3/infrastructure/webservice_http.py
@@ -27,6 +27,7 @@ def fetch_webservice_http(container_id,route_id):
             container_data.append(row)
 
         # Output
+        print("Executed successfully")
         return container_data
 
     # Failed Request
